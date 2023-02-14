0b9306464091f56c130495e34f15ac25ff4e1ae2
Roman <sayga76239@gmail.com>
2023-02-14 14:25:24 +0100

    2

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -4,12 +4,13 @@ export const StyledButton = styled.button`
   margin: 0 auto;
   padding: 8px 16px;
   border-radius: 2px;
-  background-color: #3f51b5;
+  background-color: blue;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
   display: inline-block;
   color: #fff;
   border: 0;
+  border-radius: 4px;
   text-decoration: none;
   cursor: pointer;
   font-family: inherit;
